e904946ad916a6a085d2026c49b5466d1f68e912
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2025-05-20 22:05:35 -0300

    feat: :sparkles: Add gresource availability check to extract_gst.sh

--- scripts/extract_gst.sh
+++ scripts/extract_gst.sh
@@ -3,6 +3,11 @@
 gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
 workdir=$1/shell-theme
 
+if ! command -v gresource >/dev/null 2>&1; then
+	echo "gresource is not installed. Please install it and try again."
+	exit 1
+fi
+
 for r in $(gresource list $gst); do
 	r=${r#\/org\/gnome\/shell/}
 	if [ ! -d $workdir/${r%/*} ]; then
